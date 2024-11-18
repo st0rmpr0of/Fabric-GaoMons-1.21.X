13d5eda9c0066bc3012705f49e129abafb96a783
Logan Switzer <30557653+loganswitzer01@users.noreply.github.com>
2024-11-18 07:57:30 -0600

    Initial commit

---
+++ src/main/java/net/st0rmpr0of/gaomons/GaoMonsClient.java
@@ -0,0 +1,10 @@
+package net.st0rmpr0of.gaomons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GaoMonsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
